1d0289b697dbc332bea2522b58321adc6a3e3dbc
Smuel18 <shh92533@gmail.com>
2023-11-14 11:39:17 +0200

    add product page

--- src/features/products/pages/Category.tsx
+++ src/features/products/pages/Category.tsx
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Filters from "../MUI/Filters";
 import PriceFilter from "../MUI/PriceFilter";
 import Products from "../components/Products";
+import Button from "@mui/material/Button";
 
 interface CategoryProp {
   title: string;
@@ -11,7 +12,14 @@ interface CategoryProp {
 }
 
 const Category = ({ title, filters }: CategoryProp) => {
-  const ex = ["a", "b", "c"];
+  const example = ["a", "b", "c"];
+
+  // הגדרת פונקציה לטיפול בלחיצה על הכפתור
+  const handleApplyFilterClick = () => {
+    console.log("Apply Filtering clicked!");
+    // נוסיף פעולות נוספות שתרצה לבצע כאשר הכפתור ילחץ
+  };
+
   return (
     <>
       <Box textAlign="center" marginTop={2}>
@@ -21,8 +29,16 @@ const Category = ({ title, filters }: CategoryProp) => {
         <PriceFilter />
       </Box>
       {filters.map((item, index) => (
-        <Filters key={index} headline={item} values={ex} />
+        <Filters key={index} headline={item} values={example} />
       ))}
+
+      <Button
+        style={{ backgroundColor: "your_color_code", color: "inherit" }}
+        onClick={() => handleApplyFilterClick()}
+      >
+        Apply Filtering
+      </Button>
+
       <Products />
     </>
   );
